0b7c1aa2d96071571191ceba5c3458dc65dd344f
root <1258252786@qq.com>
2020-03-06 21:19:46 +0800

    2020-3-6

--- 980. 不同路径 III/src/Solution.java
+++ 980. 不同路径 III/src/Solution.java
@@ -4,7 +4,6 @@ class Solution {
     private int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     private int R, C;
     private int[][] grid;
-    private boolean[][] visited;
     private int start, end;
 
     public int uniquePathsIII(int[][] grid) {
@@ -12,7 +11,6 @@ class Solution {
         this.grid = grid;
         R = grid.length;
         C = grid[0].length;
-        visited = new boolean[R][C];
         int left = R * C;
 
         for(int i = 0; i < R; i ++)
@@ -27,29 +25,30 @@ class Solution {
                 }
                 else if(grid[i][j] == -1)
                     left --;
-
-        return dfs(start, left);
+        int visited = 0;
+        return dfs(visited, start, left);
     }
 
-    private int dfs(int v, int left){
+    private int dfs(int visited, int v, int left){
 
         int x = v / C, y = v % C;
-        visited[x][y] = true;
+        visited += (1 << v);
         left --;
         if(v == end && left == 0){
-            visited[x][y] = false;
+            visited -= (1 << v);
             return 1;
         }
 
         int res = 0;
         for(int d = 0; d < 4; d ++){
             int nextx = x + dirs[d][0], nexty = y + dirs[d][1];
+            int next = nextx * C + nexty;
             if(inArea(nextx, nexty) && grid[nextx][nexty] == 0
-                    && !visited[nextx][nexty])
-                res += dfs(nextx * C + nexty, left);
+                    && (visited & ( 1 << next)) == 0)
+                res += dfs(visited, next, left);
         }
 
-        visited[x][y] = false;
+        visited -= (1 << v);
         return res;
     }
 
@@ -57,3 +56,74 @@ class Solution {
         return x >= 0 && x < R && y >= 0 && y < C;
     }
 }
+
+// 记忆化搜索，此题这样更慢
+/*
+import java.util.Arrays;
+
+/// Leetcode 980
+class Solution {
+
+    private int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private int R, C;
+    private int[][] grid;
+    private int start, end;
+    private int[][] memo;
+
+    public int uniquePathsIII(int[][] grid) {
+
+        this.grid = grid;
+        R = grid.length;
+        C = grid[0].length;
+        int left = R * C;
+        memo = new int[1 << R * C][R * C];
+        for (int i = 0; i < memo.length; i++){
+            Arrays.fill(memo[i],-1);
+        }
+
+        for(int i = 0; i < R; i ++)
+            for(int j = 0; j < C; j ++)
+                if(grid[i][j] == 1){
+                    start = i * C + j;
+                    grid[i][j] = 0;
+                }
+                else if(grid[i][j] == 2){
+                    end = i * C + j;
+                    grid[i][j] = 0;
+                }
+                else if(grid[i][j] == -1)
+                    left --;
+        int visited = 0;
+        return dfs(visited, start, left);
+    }
+
+    private int dfs(int visited, int v, int left){
+
+        if (memo[visited][v] != -1) return memo[visited][v];
+        int x = v / C, y = v % C;
+        visited += (1 << v);
+        left --;
+        if(v == end && left == 0){
+            visited -= (1 << v);
+            memo[visited][v] = 1;
+            return 1;
+        }
+
+        int res = 0;
+        for(int d = 0; d < 4; d ++){
+            int nextx = x + dirs[d][0], nexty = y + dirs[d][1];
+            int next = nextx * C + nexty;
+            if(inArea(nextx, nexty) && grid[nextx][nexty] == 0
+                    && (visited & ( 1 << next)) == 0)
+                res += dfs(visited, next, left);
+        }
+
+        visited -= (1 << v);
+        memo[visited][v] = res;
+        return res;
+    }
+
+    private boolean inArea(int x, int y){
+        return x >= 0 && x < R && y >= 0 && y < C;
+    }
+}*/
