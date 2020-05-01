5f1fe359d78dcd2f96d6cf2d4d8d00fc26ffccc9
root <1258252786@qq.com>
2020-05-01 18:55:38 +0800

    2020-5-1

--- 21. 合并两个有序链表/src/Solution.java
+++ 21. 合并两个有序链表/src/Solution.java
@@ -41,3 +41,34 @@ class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while(l1 != null && l2 != null){
+            if(l1.val >= l2.val){
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            } else{
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            }
+        }
+
+        if(l1 != null){
+            cur.next = l1;
+        }
+
+        if(l2 != null){
+            cur.next = l2;
+        }
+
+        return head.next;
+    }
+}
+ */
