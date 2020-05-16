d8662595d000dd98b04af7b36849ca18d9f7a973
root <1258252786@qq.com>
2020-05-16 17:07:25 +0800

    2020-5-16

--- 25. K 个一组翻转链表/src/Solution.java
+++ 25. K 个一组翻转链表/src/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
 
-        ListNode dummmyHead = new ListNode(0);
-        dummmyHead.next = head;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
 
         int length = 0;
         ListNode tmp = head;
@@ -11,7 +11,7 @@ class Solution {
             tmp = tmp.next;
         }
 
-        ListNode p = dummmyHead;
+        ListNode p = dummyHead;
         ListNode[] listNodes = new ListNode[k];
         for (int i = 0; i < length/k; i++){
             tmp = p;
@@ -28,7 +28,7 @@ class Solution {
             p = listNodes[0];
         }
 
-        return dummmyHead.next;
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
@@ -50,3 +50,43 @@ class Solution {
         }
     }
 }
+
+/*  fast
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++)
+                end = end.next;
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+}
+ */
