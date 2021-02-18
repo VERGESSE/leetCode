91c0f39d6ef65f59fde1e3e958e3bee1262b9453
haoyu <1258252786@qq.com>
2021-02-18 16:03:16 +0800

    995. K 连续位的最小翻转次数

---
+++ 995. K 连续位的最小翻转次数/src/Solution.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int minKBitFlips(int[] A, int K) {
+        int n = A.length;
+        int ans = 0, revCnt = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i >= K && A[i - K] > 1) {
+                revCnt ^= 1;
+                // 复原数组元素，若允许修改数组 A，则可以省略
+                A[i - K] -= 2;
+            }
+            if (A[i] == revCnt) {
+                if (i + K > n) {
+                    return -1;
+                }
+                ++ans;
+                revCnt ^= 1;
+                A[i] += 2;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minKBitFlips(
+                new int[]{1,1,0},
+                2
+        ));
+    }
+}
+
+/* 差分数组 需要额外空间 好理解
+class Solution {
+    public int minKBitFlips(int[] A, int K) {
+        int n = A.length;
+        int[] diff = new int[n + 1];
+        int ans = 0, revCnt = 0;
+        for (int i = 0; i < n; ++i) {
+            revCnt += diff[i];
+            if ((A[i] + revCnt) % 2 == 0) {
+                if (i + K > n) {
+                    return -1;
+                }
+                ++ans;
+                ++revCnt;
+                --diff[i + K];
+            }
+        }
+        return ans;
+    }
+}
+ */
