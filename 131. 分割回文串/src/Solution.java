5c9f22914ff48ce05d4795b6701863bb741dcf18
haoyu <1258252786@qq.com>
2021-03-07 10:47:13 +0800

    131. 分割回文串

--- 131. 分割回文串/src/Solution.java
+++ 131. 分割回文串/src/Solution.java
@@ -1,57 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    List<List<String>> ret = new ArrayList<>();
-
     public List<List<String>> partition(String s) {
-        ret.clear();
 
-        if (s.length() == 0)
+        List<List<String>> ret = new ArrayList<>();
+
+        if(s.length() == 0){
             return ret;
+        }
 
-        getPartition(s,0,new ArrayList<>());
+        getPartition(ret, s, 0, new ArrayList<>(s.length()));
 
         return ret;
     }
 
-    private void getPartition(String s,int index,List<String> res) {
-
-//        if (String.join("", res).length() == s.length()){
-        if (index == s.length()){
-//            System.out.println("--  get " + res);
-            
+    private void getPartition(List<List<String>> ret, String s,
+                              int index, List<String> res){
+        if(index == s.length()){
             ret.add(new ArrayList<>(res));
             return;
         }
-
-        for (int i = 1; i <= s.length() - index; i++){
-            if (index+i > s.length()) break;
-
-            String s1 = s.substring(index, index + i);
-            if (!isPartition(s1)) continue;
-
-            res.add(s1);
-//            System.out.println("-- add " + s1);
-
-            getPartition(s,index+i,res);
-
-            String s2 = res.remove(res.size() - 1);
-//            System.out.println("-- remove1 " + s2);
+        for(int i = index; i < s.length(); i++){
+            if(!isPalindrome(s, index, i)){
+                continue;
+            }
+            res.add(s.substring(index, i + 1));
+            getPartition(ret, s, i + 1, res);
+            res.remove(res.size() - 1);
         }
-
     }
 
-    private boolean isPartition(String s){
+    private boolean isPalindrome(String s, int start, int end){
 
-        if (s.length() == 1)
-            return true;
-
-        for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) != s.charAt(s.length() - i - 1))
+        while (start <= end){
+            if (s.charAt(start) != s.charAt(end)){
                 return false;
+            }
+            start++;
+            end--;
         }
-
         return true;
     }
 }
