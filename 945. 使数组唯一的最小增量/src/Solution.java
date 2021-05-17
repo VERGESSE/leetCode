2b2a72341e1810da20d02d8581b2fbbb5f715160
haoyu <1258252786@qq.com>
2021-05-17 23:39:40 +0800

    945. 使数组唯一的最小增量

--- 945. 使数组唯一的最小增量/src/Solution.java
+++ 945. 使数组唯一的最小增量/src/Solution.java
@@ -48,4 +48,20 @@ class Solution {
     }
 }
 
+class Solution {
+    public int minIncrementForUnique(int[] A) {
+        // 先排序
+        Arrays.sort(A);
+        int move = 0;
+        // 遍历数组，若当前元素小于等于它的前一个元素，则将其变为前一个数+1
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] <= A[i - 1]) {
+                int pre = A[i];
+                A[i] = A[i - 1] + 1;
+                move += A[i] - pre;
+            }
+        }
+        return move;
+    }
+}
  */
