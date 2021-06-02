42d882ad72cfbe630549d2fb7f5a5adb6bc9f849
haoyu <1258252786@qq.com>
2021-06-02 23:49:49 +0800

    523. 连续的子数组和

---
+++ 523. 连续的子数组和/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int m = nums.length;
+        if (m < 2) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int remainder = 0;
+        for (int i = 0; i < m; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (map.containsKey(remainder)) {
+                int prevIndex = map.get(remainder);
+                if (i - prevIndex >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.checkSubarraySum(
+                new int[]{23,2,4,6,6},
+                7));
+    }
+}
