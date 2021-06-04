2bbce8ec7b82f72c97513ecf2853d4ea9191a5d0
haoyu <1258252786@qq.com>
2021-06-04 23:22:39 +0800

    160. 相交链表

---
+++ 160. 相交链表/src/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode HA = headA, HB = headB;
+        while(HA != HB){
+            HA = HA == null ? headB : HA.next;
+            HB = HB == null ? headA : HB.next;
+        }
+        return HA;
+    }
+}
