a4ce7a40e42f0eca44af942174b475061959c94c
haoyu <1258252786@qq.com>
2021-06-14 09:19:56 +0800

    374. 猜数字大小

---
+++ 374. 猜数字大小/src/GuessGame.java
@@ -0,0 +1,10 @@
+/**
+ * @author haoyu
+ * @date 2021/6/14 9:13.
+ */
+public class GuessGame {
+
+    int guess(int num){
+        return 0;
+    }
+}
---
+++ 374. 猜数字大小/src/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+
+        int l = 1, r = n;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            int guess = guess(mid);
+            if(guess == 0){
+                return mid;
+            } else if(guess == -1){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
