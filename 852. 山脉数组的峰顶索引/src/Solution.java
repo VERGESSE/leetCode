33fd46be65e57ff7a7fbad87cad278479cb21855
haoyu <1258252786@qq.com>
2021-06-16 09:24:20 +0800

    852

---
+++ 852. 山脉数组的峰顶索引/src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+
+        int n = arr.length;
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
