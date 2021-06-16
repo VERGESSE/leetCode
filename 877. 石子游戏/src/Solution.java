bbebe320936ef5c61815f27f1ebc0a9790466335
haoyu <1258252786@qq.com>
2021-06-16 09:33:26 +0800

    877. 石子游戏

--- 877. 石子游戏/src/Solution.java
+++ 877. 石子游戏/src/Solution.java
@@ -13,3 +13,21 @@ class Solution {
         return dp[length - 1] > 0;
     }
 }
+
+/*
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int length = piles.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] > 0;
+    }
+}
+ */
