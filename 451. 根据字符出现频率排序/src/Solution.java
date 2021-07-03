32fe78c65fd9f52b671ca70fb6199285e64d6218
haoyu <1258252786@qq.com>
2021-07-03 19:49:51 +0800

    451. 根据字符出现频率排序

--- 451. 根据字符出现频率排序/src/Solution.java
+++ 451. 根据字符出现频率排序/src/Solution.java
@@ -3,6 +3,39 @@ import java.util.*;
 class Solution {
     public String frequencySort(String s) {
 
+        int[] cont = new int[256];
+        char[] chars = s.toCharArray();
+        for(char c : chars){
+            cont[c] ++;
+        }
+        List<Object[]> list = new ArrayList<>();
+        for(int i = 0; i < cont.length; i++){
+            if(cont[i] > 0){
+                StringBuilder sb = new StringBuilder(cont[i]);
+                for(int j = 0; j < cont[i]; j++){
+                    sb.append((char)i);
+                }
+                list.add(new Object[]{cont[i], sb.toString()});
+            }
+        }
+        StringBuilder sb = new StringBuilder(s.length());
+        list.stream()
+                .sorted((a, b) -> (int) b[0] - (int) a[0])
+                .map(a -> (String)a[1])
+                .forEach(sb::append);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.frequencySort("tree"));
+    }
+}
+
+/*
+class Solution {
+    public String frequencySort(String s) {
+
         Map<Character,Integer> charNumMap = new HashMap<>();
 
         for(int i = 0 ; i < s.length() ; i++){
@@ -40,3 +73,4 @@ class Solution {
         System.out.println(solution.frequencySort("tree"));
     }
 }
+ */
