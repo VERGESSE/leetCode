ab07175e78db8abb6f693bcb488f57602b942da1
haoyu <1258252786@qq.com>
2021-07-07 12:56:50 +0800

    1711. 大餐计数

--- 1711. 大餐计数/src/Solution.java
+++ 1711. 大餐计数/src/Solution.java
@@ -22,3 +22,28 @@ class Solution {
 		return (int) (res % 1_000_000_007L);
 	}
 }
+
+/*
+class Solution {
+    public int countPairs(int[] deliciousness) {
+        final int MOD = 1000000007;
+        int maxVal = 0;
+        for (int val : deliciousness) {
+            maxVal = Math.max(maxVal, val);
+        }
+        int maxSum = maxVal * 2;
+        int pairs = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = deliciousness.length;
+        for (int i = 0; i < n; i++) {
+            int val = deliciousness[i];
+            for (int sum = 1; sum <= maxSum; sum <<= 1) {
+                int count = map.getOrDefault(sum - val, 0);
+                pairs = (pairs + count) % MOD;
+            }
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        return pairs;
+    }
+}
+ */
