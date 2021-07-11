9e4708cb91cc81cddd0f87a699bb17eef9427984
haoyu <1258252786@qq.com>
2021-07-11 10:35:07 +0800

    274. H 指数

---
+++ 274. H 指数/src/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0, i = citations.length - 1;
+        while (i >= 0 && citations[i] > h) {
+            h++;
+            i--;
+        }
+        return h;
+    }
+}
+
+/*
+public class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length, tot = 0;
+        int[] counter = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n) {
+                counter[n]++;
+            } else {
+                counter[citations[i]]++;
+            }
+        }
+        for (int i = n; i >= 0; i--) {
+            tot += counter[i];
+            if (tot >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+
+ */
