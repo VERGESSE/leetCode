4c6342e58ee3dddbfaa3c494de9ed487abe10164
haoyu <1258252786@qq.com>
2021-07-15 23:13:23 +0800

    1846. 减小和重新排列数组后的最大元素

---
+++ 1846. 减小和重新排列数组后的最大元素/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] - arr[i - 1] > 0){
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+        return arr[arr.length - 1];
+    }
+}
