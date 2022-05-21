89f293843c705aabad94c328d6277fee7db6284c
haoyu <1258252786@qq.com>
2022-05-21 18:59:50 +0800

    961. 在长度 2N 的数组中找出重复 N 次的元素

--- 961. 在长度 2N 的数组中找出重复 N 次的元素/src/Solution.java
+++ 961. 在长度 2N 的数组中找出重复 N 次的元素/src/Solution.java
@@ -2,6 +2,32 @@ class Solution {
     public int repeatedNTimes(int[] nums) {
         int n = nums.length;
 
+        if (nums[0] == nums[2] || nums[0] == nums[3]){
+            return nums[0];
+        }
+        if (nums[1] == nums[3]){
+            return nums[1];
+        }
+
+        for (int i = 0; i < n - 1; i++){
+            if (nums[i] == nums[i + 1]){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.repeatedNTimes(new int[]{0,1,2,0,3,0}));
+    }
+}
+
+/*
+public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
+
         for (int i = 0; i < n - 3; i++){
             if(nums[i] == nums[i + 1] || nums[i] == nums[i + 2] || nums[i] == nums[i + 3]){
                 return nums[i];
@@ -16,9 +42,4 @@ class Solution {
 
         return -1;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.repeatedNTimes(new int[]{1,2,3,3}));
-    }
-}
+ */
