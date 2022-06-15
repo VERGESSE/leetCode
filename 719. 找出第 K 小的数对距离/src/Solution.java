6fa6feedc14691265cc246e0e9ec59a8d85975c3
haoyu <1258252786@qq.com>
2022-06-15 15:06:01 +0800

    719. 找出第 K 小的数对距离

---
+++ 719. 找出第 K 小的数对距离/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length, left = 0, right = nums[n - 1] - nums[0];
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int cnt = 0;
+            for (int i = 0, j = 0; j < n; j++) {
+                while (nums[j] - nums[i] > mid) {
+                    i++;
+                }
+                cnt += j - i;
+            }
+            if (cnt >= k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
