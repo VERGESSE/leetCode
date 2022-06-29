af522c384628a0dfbaba66480dbb5a5d7992d1f8
haoyu <1258252786@qq.com>
2022-06-29 12:36:42 +0800

    535. TinyURL 的加密与解密

---
+++ 535. TinyURL 的加密与解密/src/Codec.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Codec {
+    private Map<Integer, String> dataBase = new HashMap<Integer, String>();
+    private int id;
+
+    public String encode(String longUrl) {
+        id++;
+        dataBase.put(id, longUrl);
+        return "http://tinyurl.com/" + id;
+    }
+
+    public String decode(String shortUrl) {
+        int p = shortUrl.lastIndexOf('/') + 1;
+        int key = Integer.parseInt(shortUrl.substring(p));
+        return dataBase.get(key);
+    }
+}
